ab96e46bef808c2b2282646589c3f33798063fb0
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-26 17:02:58 -0300

    chore: add personal rules to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,41 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:testing-library/react'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'sort-keys-fix', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'simple-import-sort/exports': 'error',
+    'sort-keys': ['error', 'asc', { caseSensitive: true, natural: false, minKeys: 2 }]
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              ['^react', '^@?\\w'],
+              ['(@|components)(/.*|$)'],
+              ['^\\u0000'],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              ['^.+\\.?(scss)$']
+            ]
+          }
+        ]
+      }
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'sort-keys-fix/sort-keys-fix': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn'
+      }
+    }
+  ]
 }
