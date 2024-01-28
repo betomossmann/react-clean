433d376aad04db193b3820079ee5e09cb0e37edb
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-28 09:12:07 -0300

    chore: add axios and autoimports vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), AutoImport({ dts: true, imports: ['vitest'] })],
   test: {
     // alias: {
     //   '@/': new URL('./src/$1', import.meta.url).pathname,
